510020b93d342905fa84abe6a03c819801d2be88
Joey Whalen <jtktwhalen@gmail.com>
2021-01-30 15:31:34 -0500

    updated Hello World file

---
+++ src/javaControlFlow2TTSHW/Main.java
@@ -0,0 +1,10 @@
+package javaControlFlow2TTSHW;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/javaControlFlow2TTSHW/helloWorld.java
@@ -0,0 +1,12 @@
+package javaControlFlow2TTSHW;
+
+public class helloWorld {
+
+	public static void main(String[] args) {
+		// TODO print Hello World
+		String text = "Hello World";
+		System.out.println(text);
+		
+	}
+
+}
